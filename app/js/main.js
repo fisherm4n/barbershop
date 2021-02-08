afef867dc91fd4d1e9df2e81ab5ecbf48d2309cf
fisherm4n <4143322@inbox.ru>
2021-02-08 10:26:08 +0300

    started adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,38 @@
 
 $(function (){
-   const swiper = new Swiper('.swiper-container', {
-    
-  slidesPerView: 4,
-      spaceBetween: 30,
-      
-});
+   var swiper = new Swiper('.swiper-container', {
+     slidesPerView: 1,
+      spaceBetween: 10,
+      // init: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      }
+    });
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() >550) {
+      $(".menu").addClass("fixed");
+      $(".menu__item-b--call").addClass("active");
+    } else{
+      $(".menu").removeClass("fixed");
+      $(".menu__item-b--call").removeClass("active");
 
+    }
+  });
   $('.price__item').on('click', function (e) {
     e.preventDefault();
     $('.price__item').removeClass('price__item--active');
@@ -15,6 +41,8 @@ $(function (){
     $($(this).attr('href')).addClass('price__content-item--active');
 
   });
- 
    $("#phone").mask("(999) 999-9999");
+   $('.menu__btn').on('click',function () {
+     $('.menu__list').toggleClass('menu__list--active');
+   });
 });
