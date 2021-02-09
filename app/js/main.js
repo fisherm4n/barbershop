2cfd31d12150bf34c420cb4221d083cc1beb37df
fisherm4n <4143322@inbox.ru>
2021-02-10 01:55:08 +0300

    almost finished adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,26 @@
 
 $(function (){
    var swiper = new Swiper('.swiper-container', {
-     slidesPerView: 1,
-      spaceBetween: 10,
+    spaceBetween: 10,
       // init: false,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+     navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
       breakpoints: {
-        640: {
+        500: {
           slidesPerView: 2,
-          spaceBetween: 20,
+           
         },
         768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
+          slidesPerView: 3,
         },
         1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
+          slidesPerView: 4,
         },
       }
     });
+    
   $(window).on("scroll", function () {
     if ($(this).scrollTop() >550) {
       $(".menu").addClass("fixed");
