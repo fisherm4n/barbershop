993f9c0952f48d11d37c91c6f48a65333bd041fa
fisherm4n <4143322@inbox.ru>
2021-02-10 19:16:30 +0300

    finished project

--- app/js/main.js
+++ app/js/main.js
@@ -31,6 +31,19 @@ $(function (){
 
     }
   });
+   $(".menu__link, .more__signup").on("click",function (e) {
+		//отменяем стандартную обработку нажатия по ссылке
+		e.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+  });
   $('.price__item').on('click', function (e) {
     e.preventDefault();
     $('.price__item').removeClass('price__item--active');
